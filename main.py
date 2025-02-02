a4d4d9d54f9b362197758aab4ac67886003f7da9
PAVIN7410 <wowka1074@gmail.com>
2025-02-03 00:05:20 +0300

    add function area square.

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ def area_circle(r):
 radius = float(input("Введи радиус: "))
 s = area_circle(radius)
 print(f"Площадь круга = {s}")
+
+
+def area_square_area(a):
+    S_s = a ** 2
+    return S_s
+
+a = float(input("Введи сторону квадрата: "))
+
+S_s = area_square_area(a)
+print(f"Площадь квадрата = {S_s}")
